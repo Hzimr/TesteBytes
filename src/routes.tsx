e6266ef6f8b9be66d62ea8347fa3da1580168758
Hítalo Rodrigues <hitalo005@gmail.com>
2024-05-31 16:12:41 -0300

    feat: created page home

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,10 +1,17 @@
 import { createBrowserRouter } from 'react-router-dom'
 
 import { Fulampo } from './pages/app/fulampos'
+import { Home } from './pages/home/home'
 
 export const router = createBrowserRouter([
   {
     path: '/',
-    element: <Fulampo />,
+    element: <Home />,
+    children: [
+      {
+        path: '/fulampos',
+        element: <Fulampo />,
+      },
+    ],
   },
 ])
