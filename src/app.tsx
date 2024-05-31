56e33985dfe6eac50ec79cf047ce87a8c38cbac7
Hítalo Rodrigues <hitalo005@gmail.com>
2024-05-31 16:55:41 -0300

    feat: used helmet in app and import globals.css

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,15 @@
+import './globals.css'
+
+import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { RouterProvider } from 'react-router-dom'
 
 import { router } from './routes'
 
 export function App() {
-  return <RouterProvider router={router} />
+  return (
+    <HelmetProvider>
+      <Helmet titleTemplate="%s | Teste Bytes" />
+      <RouterProvider router={router} />
+    </HelmetProvider>
+  )
 }
